39d64b2778f82521b37715e9e4a97ddccab3afc5
Softands <besseausamuel@gmail.com>
2024-02-05 19:13:16 +0100

    up

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -45,36 +45,48 @@ clientsClaim();
 // Their URLs are injected into the manifest variable below.
 // This variable must be present somewhere in your service worker file,
 // even if you decide not to use precaching. See https://cra.link/PWA
-precacheAndRoute(self.__WB_MANIFEST);
+// precacheAndRoute(self.__WB_MANIFEST);
+
+
+const toPrecache = self.__WB_MANIFEST.filter(
+  (file) => typeof file !== 'string' && !file.url.includes('index.html'),
+)
+
+precacheAndRoute(toPrecache)
+
+registerRoute(
+  ({ url }) => url.pathname.includes('index.html'),
+  new NetworkFirst(),
+)
 
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
-registerRoute(
-  // Return false to exempt requests from being fulfilled by index.html.
-  ({ request, url }: { request: Request; url: URL }) => {
-    // If this isn't a navigation, skip.
-    if (request.mode !== "navigate") {
-      return false;
-    }
+// const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
+// registerRoute(
+//   // Return false to exempt requests from being fulfilled by index.html.
+//   ({ request, url }: { request: Request; url: URL }) => {
+//     // If this isn't a navigation, skip.
+//     if (request.mode !== "navigate") {
+//       return false;
+//     }
 
-    // If this is a URL that starts with /_, skip.
-    if (url.pathname.startsWith("/_")) {
-      return false;
-    }
+//     // If this is a URL that starts with /_, skip.
+//     if (url.pathname.startsWith("/_")) {
+//       return false;
+//     }
 
-    // If this looks like a URL for a resource, because it contains
-    // a file extension, skip.
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    }
+//     // If this looks like a URL for a resource, because it contains
+//     // a file extension, skip.
+//     if (url.pathname.match(fileExtensionRegexp)) {
+//       return false;
+//     }
 
-    // Return true to signal that we want to use the handler.
-    return true;
-  },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
-);
+//     // Return true to signal that we want to use the handler.
+//     return true;
+//   },
+//   createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
+// );
 
 // Disable caching for specific URLs
 // registerRoute(
